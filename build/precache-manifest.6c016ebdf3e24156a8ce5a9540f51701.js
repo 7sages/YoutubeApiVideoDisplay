b7e1797998005eadfe9383af4d6acf07a23a5b8e
7sages <54614488+7sages@users.noreply.github.com>
2019-09-24 20:06:56 +0530

    Add files via upload

---
+++ build/precache-manifest.6c016ebdf3e24156a8ce5a9540f51701.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc9c4ab801470c16c1570591a0bb3c6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf28f06f075a1fe72048",
+    "url": "/static/css/main.76f9f2b5.chunk.css"
+  },
+  {
+    "revision": "02299d3f0377f0863785",
+    "url": "/static/js/2.869a05e9.chunk.js"
+  },
+  {
+    "revision": "bf28f06f075a1fe72048",
+    "url": "/static/js/main.1fac3aca.chunk.js"
+  },
+  {
+    "revision": "ebaf6c36abb6d6bda7f6",
+    "url": "/static/js/runtime-main.bd7c5796.js"
+  }
+]);
